95f20232c0658893752f02d4d73101345e9cb27f
Matthew Shaylor <github@shaylor.co.uk>
2020-06-14 23:56:24 +0100

    Update test_realenv.py

--- tests/integration/test_realenv.py
+++ tests/integration/test_realenv.py
@@ -5,6 +5,7 @@ from listentothis import env
 def test_noop():
     assert True
 
+
 def test_env():
     env.setup_gcloud_json("allsecretsjson")
-    assert(env.get("id") == "allsecretsjson")
+    assert env.get("id") == "allsecretsjson"
