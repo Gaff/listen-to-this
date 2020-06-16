e9058d70be8bf9c81db1ff7e8ee29d65dfec0cf0
Matthew Shaylor <github@shaylor.co.uk>
2020-06-16 07:27:00 +0100

    Update test_realenv.py

--- tests/integration/test_realenv.py
+++ tests/integration/test_realenv.py
@@ -6,7 +6,7 @@ from listentothis import env, Reddit
 env.setup_gcloud_json("allsecretsjson")
 assert env.get("id") == "allsecretsjson"
 
-def test_listentothis():
+def test_reddit():
     ltt = Reddit()
     data = ltt.query_reddit()
     data = list(data)
